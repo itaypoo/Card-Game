b9d126eaf604e0e274656e8a2a65e2754d8bd797
itaypoo <87711451+itaypoo@users.noreply.github.com>
2021-07-20 19:56:44 +0300

    card list

---
+++ card_list.gd
@@ -0,0 +1,49 @@
+extends Node
+
+# Base cards (Boss Types)
+# ----------------------------
+# Robot (moves up and down)
+#     Base HP: 600 
+# Frog (jumps on the player)
+#     Base HP: 450 
+# Goat (moves up and down and is small (goes WHAAAAAA)
+#     Base HP: 550 
+# Balon (rams into you)
+#     Base HP: 400 
+# Chicken (moves randomally and poops out eggs)
+#     Base HP: 450 
+# Squid (moves randomally and leaves a ink trail)
+#     Base HP: 500
+
+# Card Type 1 - Boss modifier cards
+# -----------------------------------
+# Poops out minions
+# Breaths fire
+# Shoots lazers
+# Shoots rockets
+# Throws spider webs
+# Moves fast
+# More HP
+# Less HP
+
+# Card Type 2 - Player modifier cards
+# -----------------------------------
+# Inverted controlls
+# Super fucking fast
+# Is slippery
+# Has more HP
+# Shutgun
+# Machine Gun
+# Sniper rifle
+# Seeking bullets
+
+# Card Type 3 - Global modifier cards
+# -----------------------------------
+# Random pictures of goats pop up
+# Raining exploading pigs
+# On drugs // screen is colorfull
+# Screen is pixelated
+# Duplicated screens
+# Thunder storm
+# Fast Motion
+# Slow Motion
