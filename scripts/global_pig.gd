2ca1d009706c2caee355fd07a99c3acc3813d062
itaypoo <87711451+itaypoo@users.noreply.github.com>
2021-07-23 12:06:29 +0300

    FIXED YOUR FUCKING CODE BITCH
    it was still broken so i redid it

--- scripts/global_pig.gd
+++ scripts/global_pig.gd
@@ -1,42 +1,37 @@
 extends Area2D
 
 var lifetime
-var delay_kill = -1
-
-var kill = false
+var dead = false
 
 func _ready():
 	randomize()
 	position = Vector2(int (rand_range(50, 1230)), -50)
 	randomize()
 	lifetime = int (rand_range(50, 200))
-	
+
+
 func _physics_process(_delta):
-	if kill and delay_kill >= 0:
-		delay_kill -= 1
-	elif kill:
-		queue_free()
-	if not kill:
-		position.y += 3
-		lifetime -= 1
-		if lifetime > 0:
-			for area in get_overlapping_areas():
-				if area.is_in_group("player"):
-					boom()
-		elif lifetime == 0:
-			boom()
-		
-
-func boom():
-	$circle_coll.disabled = false
-	print('')
-	$Timer.start(0.1)
+	if dead: return
+	
+	position.y += 3
+	lifetime -= 1
+	for area in get_overlapping_areas(): if area.is_in_group("player"):
+		expload()
+	if lifetime <= 0:
+		expload()
 
 
-func _on_Timer_timeout():
-	for area in get_overlapping_areas():
-		if area.is_in_group("player"):
-			area.hurt_player(1)
+func expload():
+	dead = true
 	$sprite.visible = false
-	delay_kill = 180
-	kill = true
+	$circle_coll.disabled = false
+	$coll_timer.start()
+
+
+func _on_coll_timer_timeout():
+	for area in get_overlapping_areas(): if area.is_in_group("player"):
+		area.hurt_player(1)
+	$death_timer.start()
+
+
+func _on_death_timer_timeout(): queue_free()
